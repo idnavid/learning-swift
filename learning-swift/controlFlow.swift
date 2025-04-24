1ed6b92948470d902e2a094a167bc63a16801033
navidshokouhi <navid.shokouhi@globalkineticscorp.com>
2025-04-25 05:36:24 +1000

    some examples for loops

---
+++ learning-swift/controlFlow.swift
@@ -0,0 +1,46 @@
+//
+//  controlFlow.swift
+//  learning-swift
+//
+//  Created by Navid Shokouhi on 25/4/2025.
+//
+
+/// print names  provided in a list.
+func forLoopNames() {
+    let names = ["Alice", "Bob", "Charlie"]
+    for name in names {
+        print("Hello, \(name)!")
+    }
+}
+
+/// print integers in a range
+/// ```
+/// forLoopRange() // prints a value from 1 through 5
+/// ```
+///
+/// - Parameters:
+///     - num: end of range
+func forLoopRange(till num: Int = 5) {
+    for indx in 1...num {
+        print("Index is \(indx)")
+    }
+}
+
+func forLoopStrideTo(start from: Int = 1, to: Int = 5, step by: Int = 2) {
+    for indx in stride(from: 1, to: 5, by: 2) {
+        print("Index is \(indx)")
+    }
+}
+
+func forLoopStrideThrough(start from: Int = 1, through: Int = 5, step by: Int = 2) {
+    for indx in stride(from: 1, through: 5, by: 2) {
+        print("Index is \(indx)")
+    }
+}
+
+func iterDictionary() {
+    let numberOfPets = ["Alice": 2, "Bob": 1, "Charlie": 3]
+    for (name, petCount) in numberOfPets {
+        print("\(name) has \(petCount) pets.")
+    }
+}
--- learning-swift/main.swift
+++ learning-swift/main.swift
@@ -5,7 +5,19 @@
 //  Created by Navid Shokouhi on 25/4/2025.
 //
 
-import Foundation
+print("Loop through a list of names")
+forLoopNames()
 
-print("Hello, World!")
+print("Loop through a range of numbers")
+forLoopRange()
 
+print("Loop through a range of numbers with custom size")
+forLoopRange(till: 3)
+
+print("Loop through a range of numbers with custom size and stride")
+forLoopStrideTo(to: 3)
+
+print("Loop through a closed range of numbers with custom size and stride")
+forLoopStrideThrough(through: 3)
+
+iterDictionary()
